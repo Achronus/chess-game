33cfa87d103b705b0ab76581ba77a3183fd8ae1e
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-10-19 20:49:36 +0100

    Added piece image logic

---
+++ UI/Images.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using Chess.Core;
+using Chess.Pieces;
+
+namespace UI
+{
+    public static class Images
+    {
+        private static Dictionary<PieceType, ImageSource> WhitePieces { get; } = new()
+        {
+            { PieceType.Pawn, LoadImage("assets/white/pawn.png") },
+            { PieceType.Rook, LoadImage("assets/white/rook.png") },
+            { PieceType.Knight, LoadImage("assets/white/knight.png") },
+            { PieceType.Bishop, LoadImage("assets/white/bishop.png") },
+            { PieceType.Queen, LoadImage("assets/white/queen.png") },
+            { PieceType.King, LoadImage("assets/white/king.png") }
+        };
+        private static Dictionary<PieceType, ImageSource> BlackPieces { get; } = new()
+        {
+            { PieceType.Pawn, LoadImage("assets/black/pawn.png") },
+            { PieceType.Rook, LoadImage("assets/black/rook.png") },
+            { PieceType.Knight, LoadImage("assets/black/knight.png") },
+            { PieceType.Bishop, LoadImage("assets/black/bishop.png") },
+            { PieceType.Queen, LoadImage("assets/black/queen.png") },
+            { PieceType.King, LoadImage("assets/black/king.png") }
+        };
+
+        private static ImageSource LoadImage(string filepath)
+        {
+            return new BitmapImage(new Uri(filepath, UriKind.Relative));
+        }
+        
+        public static ImageSource GetImage(Colour colour, PieceType type)
+        {
+            return colour switch
+            {
+                Colour.White => WhitePieces[type],
+                Colour.Black => BlackPieces[type],
+                _ => throw new ArgumentException("Invalid colour")
+            };
+        }
+
+        public static ImageSource? GetImage(Piece piece)
+        {
+            return piece != null ? GetImage(piece.Colour, piece.Type) : null;
+        }
+    }
+}
