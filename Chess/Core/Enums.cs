7370ae8d44204caac49db9df8dc9c45307357c88
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-10-19 21:14:10 +0100

    Added `Move` base class with `MoveType` enum

--- Chess/Core/Enums.cs
+++ Chess/Core/Enums.cs
@@ -1,30 +1,40 @@
 ﻿namespace Chess.Core
 {
-  public enum Colour
-  {
-    White,
-    Black,
-  }
+    public enum Colour
+    {
+        White,
+        Black
+    }
 
-  public enum PieceType
-  {
-    Pawn,
-    Rook,
-    Knight,
-    Bishop,
-    King,
-    Queen
-  }
+    public enum PieceType
+    {
+        Pawn,
+        Rook,
+        Knight,
+        Bishop,
+        King,
+        Queen
+    }
 
-  public enum DirectionType
-  {
-    NorthEast,
-    North,
-    NorthWest,
-    East,
-    SouthEast,
-    South,
-    SouthWest,
-    West
-  }
+    public enum DirectionType
+    {
+        NorthEast,
+        North,
+        NorthWest,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West
+    }
+
+    public enum MoveType
+    { 
+        Normal,
+        CastleShort,
+        CastleLong,
+        PawnDoubleMove,
+        EnPassant,
+        PawnPromotion
+    }
 }
