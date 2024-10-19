fdaa81a1bd09fd378f27e1cde1ef184b23bcdd9b
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-10-19 23:07:51 +0100

    Add directions for `King`, `Pawn`

--- Chess/Pieces/King.cs
+++ Chess/Pieces/King.cs
@@ -1,4 +1,5 @@
 using Chess.Core;
+using Chess.Moves;
 
 namespace Chess.Pieces
 {
@@ -7,6 +8,21 @@ namespace Chess.Pieces
         public override Colour Colour => colour;
         public override PieceType Type => PieceType.King;
         public override int ScoreValue => 0;
+        public override Vector2D[] Directions => [
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West,
+            Direction.NorthWest,
+            Direction.NorthEast,
+            Direction.SouthWest,
+            Direction.SouthEast
+        ];
+
+        public override IEnumerable<Move> GetMoves(Vector2D from, Board board)
+        {
+            throw new NotImplementedException();
+        }
 
         public override void Move()
         {
--- Chess/Pieces/Pawn.cs
+++ Chess/Pieces/Pawn.cs
@@ -1,4 +1,5 @@
 using Chess.Core;
+using Chess.Moves;
 
 namespace Chess.Pieces
 {
@@ -7,6 +8,14 @@ namespace Chess.Pieces
         public override Colour Colour => colour;
         public override PieceType Type => PieceType.Pawn;
         public override int ScoreValue => 1;
+        public override Vector2D[] Directions => [
+            Colour == Colour.White ? Direction.North : Direction.South,
+        ];
+
+        public override IEnumerable<Move> GetMoves(Vector2D from, Board board)
+        {
+            throw new NotImplementedException();
+        }
 
         public override void Move()
         {
